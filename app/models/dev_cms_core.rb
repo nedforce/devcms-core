d00933b7ef42d44d95edb7efab1b45a254fc9c88
Bas Veeling <b.veeling@nedforce.nl>
2011-02-25 15:48:34 +0100

    Add module registration to DevCMSCore

--- app/models/dev_cms_core.rb
+++ app/models/dev_cms_core.rb
@@ -1,5 +1,16 @@
 class DevCMSCore
+  @@modules = Array.new()
+  
   def self.root
     File.join(File.dirname(__FILE__), "..", "..")
   end
+  
+  def self.register_module(moduleName)
+    @@modules << moduleName
+  end
+  
+  def self.is_registered?(moduleName)
+    @@modules.include?(moduleName)
+  end
+  
 end
