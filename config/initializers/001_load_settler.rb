20917993a757e3f414126711425fddd106c58805
Roel van der Hoorn <vanderhoorn@gmail.com>
2011-03-22 12:06:06 +0100

    Typo fix.

--- config/initializers/001_load_settler.rb
+++ config/initializers/001_load_settler.rb
@@ -1,2 +1,2 @@
-# Load Settler manualy to ensure all settings are directly available. 
+# Load Settler manually to ensure all settings are directly available. 
 SETTLER_LOADED = !(Settler.load! rescue nil).nil?
