7906968acb2a464761255ee130ea4d6aa5bf2319
Bas Veeling <b.veeling@nedforce.nl>
2011-06-06 11:58:41 +0200

    Digitaal loket blocks geupdate

--- public/javascripts/treemenu.js
+++ public/javascripts/treemenu.js
@@ -4,14 +4,42 @@ document.observe('dom:loaded', function() {
 
 function javascriptifyTreeMenus() {
   $$('ul.treemenu_js').each(function(treemenu) {
-		$$('li').each(function(treeitem) {
-    	treeitem.observe('click', function(event) {
-	      toggleTreeItem(treeitem);
+		treemenu.childElements().each(function(treeitem) {
+			treeitem.firstDescendant().observe('click', function(event) {
+	      toggleTreeItem(treemenu,treeitem);
 	      event.stop();
 	    });
+    	/*treeitem.observe('click', function(event) {
+	      toggleTreeItem(treemenu,treeitem);
+	      event.stop();
+	    });*/
+		});
   });
 }
 
-function toggleTreeItem(treeitem) {
-	alert('toggle');
+function toggleTreeItem(treemenu,treeitem) {
+	plus = treeitem.firstDescendant();
+	if(plus.innerHTML == '-') {
+		collapse = true;
+	} else {
+		collapse = false;
+	}
+	treemenu.getElementsBySelector('li ul').each(function(item) {
+		item.setStyle({'display':'none'});
+	});
+	treemenu.getElementsBySelector('.tree_plus').each(function(item) {
+		item.update('+');
+	});
+	
+	if(!collapse) {
+		plus.update('-');
+		treeitem.getElementsBySelector('.branch').each(function(item) {
+			item.setStyle({'display':'block'});
+		});
+	} else {
+		plus.update('+');
+		treeitem.getElementsBySelector('.branch').each(function(item) {
+			item.setStyle({'display':'none'});
+		});
+	}
 }
