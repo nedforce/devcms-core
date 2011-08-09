e6ba9920e6e7241511c05e4d772272bf17ffe8fd
Anton Timmermans <timmermansanton@gmail.com>
2011-08-09 10:56:28 +0200

    Fixed it once and for all

--- init.rb
+++ init.rb
@@ -5,14 +5,9 @@ plugin_root = File.dirname(__FILE__)
 # Add additional load paths for your own custom dirs
 ActiveSupport::Dependencies.load_paths << "#{plugin_root}/app/sweepers"
 
-require 'pp'
-pp 'xbx'
-pp File.basname($0)
-pp ARGV
-
 Dir["#{plugin_root}/config/initializers/**/*.rb"].sort.each do |initializer|
   require(initializer)
-end  unless ( File.basename($0) == "rake" && (ARGV.include?("devcms:install") || ARGV.include?("devcms:public") || ARGV.include?("devcms:config") || ARGV.include?("devcms:app") || ARGV.include?("devcms:db")) )
+end if FileTest.exist?("#{RAILS_ROOT}/config/initializers/001_share_point_settings.rb")
 
 if Rails.env.development?
    ActiveSupport::Dependencies.load_once_paths.reject!{|x| x =~ /^#{Regexp.escape(File.dirname(__FILE__))}/}
