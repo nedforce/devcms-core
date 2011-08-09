8b5db69f600970ddc3a2d6dc7e88857a3ada7e96
Anton Timmermans <timmermansanton@gmail.com>
2011-08-09 10:34:32 +0200

    Fixed the install script loading the environment in the install script

--- init.rb
+++ init.rb
@@ -5,9 +5,14 @@ plugin_root = File.dirname(__FILE__)
 # Add additional load paths for your own custom dirs
 ActiveSupport::Dependencies.load_paths << "#{plugin_root}/app/sweepers"
 
+require 'pp'
+pp 'xbx'
+pp File.basname($0)
+pp ARGV
+
 Dir["#{plugin_root}/config/initializers/**/*.rb"].sort.each do |initializer|
   require(initializer)
-end
+end  unless ( File.basename($0) == "rake" && (ARGV.include?("devcms:install") || ARGV.include?("devcms:public") || ARGV.include?("devcms:config") || ARGV.include?("devcms:app") || ARGV.include?("devcms:db")) )
 
 # if Rails.env.development?
 #   ActiveSupport::Dependencies.load_once_paths.reject!{|x| x =~ /^#{Regexp.escape(File.dirname(__FILE__))}/}
