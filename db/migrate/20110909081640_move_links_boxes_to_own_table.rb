12d6e5135ec73cab3292a1bd4d9d9a6fbc826138
Roel van der Hoorn <vanderhoorn@gmail.com>
2011-10-12 12:06:34 +0200

    Ensure the migration works on production as well.

--- db/migrate/20110909081640_move_links_boxes_to_own_table.rb
+++ db/migrate/20110909081640_move_links_boxes_to_own_table.rb
@@ -1,11 +1,13 @@
 class MoveLinksBoxesToOwnTable < ActiveRecord::Migration
-  
-  class Section < ActiveRecord::Base
+
+  unless Rails.env.production?
+    class Section < ActiveRecord::Base
+    end
+
+    class LinksBox < ActiveRecord::Base
+    end
   end
-  
-  class LinksBox < ActiveRecord::Base
-  end
-  
+
   def self.up      
     create_table :links_boxes do |t|
       t.string  :title, :null => false
@@ -16,20 +18,20 @@ class MoveLinksBoxesToOwnTable < ActiveRecord::Migration
 
     Section.find_each(:conditions => { :type => 'LinksBox' }) do |links_box|
       new_id = ActiveRecord::Base.connection.insert("INSERT INTO links_boxes (title, description, created_at, updated_at) VALUES ('#{links_box.title}', '#{links_box.description}', '#{links_box.created_at}', '#{links_box.updated_at}')")
-      
+
       Node.update_all("content_id = #{new_id}, content_type = 'LinksBox'", "content_id = #{links_box.id} AND content_type = 'Section'")
     end
-    
+
     Section.delete_all("type = 'LinksBox'")
   end
 
   def self.down    
     LinksBox.find_each do |links_box|
       new_id = ActiveRecord::Base.connection.insert("INSERT INTO sections (type, title, description, created_at, updated_at) VALUES ('LinksBox', '#{links_box.title}', '#{links_box.description}', '#{links_box.created_at}', '#{links_box.updated_at}')")
-      
+
       Node.update_all("content_id = #{new_id}, content_type = 'Section'", "content_id = #{links_box.id} AND content_type = 'LinksBox'")
     end
-    
+
     drop_table :links_boxes
   end
 end
