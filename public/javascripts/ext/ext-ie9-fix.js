8dcd4d7dbf0f64918f1733339ca0afdadf649d99
Roel van der Hoorn <vanderhoorn@gmail.com>
2011-10-14 12:28:57 +0200

    Fix rendering of the tree in the admin part by patching Ext (part of #5538).

---
+++ public/javascripts/ext/ext-ie9-fix.js
@@ -0,0 +1,9 @@
+if ((typeof Range !== "undefined") && !Range.prototype.createContextualFragment) {
+  Range.prototype.createContextualFragment = function (html) {
+    var frag = document.createDocumentFragment(),
+    div = document.createElement("div");
+    frag.appendChild(div);
+    div.outerHTML = html;
+    return frag;
+  };
+}
