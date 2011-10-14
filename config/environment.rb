948b6ae1898bd8d7cdb2dfb5743716c1648562c9
Cas Ebbers <c.ebbers@nedforce.nl>
2011-10-14 15:58:00 +0200

    set whenver lib to false

--- config/environment.rb
+++ config/environment.rb
@@ -26,5 +26,5 @@ config.gem 'settler',               :version => '~> 1.2.0'
 config.gem 'shuber-sortable',       :version => "~> 1.0.6", :lib => 'sortable',       :source => "http://gems.github.com"
 # use the plugin instead, shuber isn't available as gem anymore
 config.gem 'tidy',                  :version => '1.1.2'
-config.gem 'whenever',              :version => '>= 0.5.0'
+config.gem 'whenever',              :version => '>= 0.5.0', :lib => false
 config.gem 'spreadsheet',           :version => '~> 0.6.5.2'
