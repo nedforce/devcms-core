97c6f7a7381c5803c8a0472fab563b806ce9c886
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2011-11-04 17:48:38 +0100

    update libxml version

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ config.gem 'dynamic_attributes',    :version => '~> 1.1.3'
 config.gem 'fastercsv'
 config.gem 'feed-normalizer',       :version => '~> 1.5.2'
 config.gem 'haml',                  :version => '~> 3.0'
-config.gem 'libxml-ruby',           :version => "~> 1.1.0", :lib => 'libxml'
+config.gem 'libxml-ruby',           :version => "~> 2.2.0", :lib => 'libxml'
 config.gem 'pg',                    :version => '~> 0.11.0'
 config.gem 'settler',               :version => '~> 1.2.0'
 config.gem 'shuber-sortable',       :version => "~> 1.0.6", :lib => 'sortable',       :source => "http://gems.github.com"
