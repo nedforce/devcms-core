28890a1cef64189fcd834b539ecdc7b37416a88a
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2011-12-19 17:43:50 +0100

    fix caching and sweeping

--- app/sweepers/node_sweeper.rb
+++ app/sweepers/node_sweeper.rb
@@ -19,17 +19,17 @@ protected
     content = node.content
     
     if Node.content_type_configuration(node.content_type)[:show_in_menu] || Node.content_type_configuration(node.sub_content_type)[:show_in_menu]
-      expire_fragment(:controller => '/nodes', :action => :footer, :site => node.containing_site.id) if node.ancestry_depth <= 1
-      expire_fragment(:controller => '/nodes', :action => :main_menu, :site => node.containing_site.id) if node.ancestry_depth <= 2  
+      controller.expire_fragment(:host => Settler[:host], :controller => '/nodes', :action => :footer, :site => node.containing_site.id) if node.ancestry_depth <= 1
+      controller.expire_fragment(:host => Settler[:host], :controller => '/nodes', :action => :main_menu, :site => node.containing_site.id) if node.ancestry_depth <= 2  
     end
     
     if content.is_a?(Image)
-      expire_page(:controller => "/images", :action => :thumbnail, :id => content.id, :format => 'jpg')
-      expire_page(:controller => "/images", :action => :header, :id => content.id, :format => 'jpg')
-      expire_page(:controller => "/images", :action => :big_header, :id => content.id, :format => 'jpg')
-      expire_page(:controller => "/images", :action => :full, :id => content.id, :format => 'jpg')
-      expire_page(:controller => "/images", :action => :sidebox, :id => content.id, :format => 'jpg')
-      expire_page(:controller => "/images", :action => :content_box_header, :id => content.id, :format => 'jpg')
+      controller.expire_page(:host => Settler[:host], :controller => "/images", :action => :thumbnail, :id => content.id, :format => 'jpg')
+      controller.expire_page(:host => Settler[:host], :controller => "/images", :action => :header, :id => content.id, :format => 'jpg')
+      controller.expire_page(:host => Settler[:host], :controller => "/images", :action => :big_header, :id => content.id, :format => 'jpg')
+      controller.expire_page(:host => Settler[:host], :controller => "/images", :action => :full, :id => content.id, :format => 'jpg')
+      controller.expire_page(:host => Settler[:host], :controller => "/images", :action => :sidebox, :id => content.id, :format => 'jpg')
+      controller.expire_page(:host => Settler[:host], :controller => "/images", :action => :content_box_header, :id => content.id, :format => 'jpg')
     end
   end
 end
--- lib/cache_extensions.rb
+++ lib/cache_extensions.rb
@@ -1,13 +1,16 @@
 module ActionView #:nodoc:
- class Base #:nodoc:
-   # adds support for expiring of cached fragment
-   def cache(key, options = {}, &block)
-     begin
-       super(key, options, &block) 
-     rescue Memcached::ServerIsMarkedDead => e
-       BackgroundNotifier.deliver_exception_notification(e, "Caching", nil)
-       yield
-     end
-   end
- end
+  class Base #:nodoc:
+    # adds support for expiring of cached fragment
+    def cache(key, options = {}, &block)
+      expires_in = key.delete(:expires_in)
+      key[:host] = Settler[:host]
+      options[:expires_in] = expires_in.to_i if expires_in.present?
+      begin
+        super(key, options, &block) 
+      rescue Memcached::ServerIsMarkedDead => e
+        BackgroundNotifier.deliver_exception_notification(e, "Caching", nil)
+        yield
+      end
+    end
+  end
 end
