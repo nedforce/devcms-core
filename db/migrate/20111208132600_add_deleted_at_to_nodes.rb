0a7f43e05ec0654b6ccc65d7ec45dc0f0f7eac74
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-01-20 14:01:23 +0100

    [DB] Fix migration sequence by improving the migration for adding deleted_at to nodes.

--- db/migrate/20111208132600_add_deleted_at_to_nodes.rb
+++ db/migrate/20111208132600_add_deleted_at_to_nodes.rb
@@ -1,8 +1,11 @@
 class AddDeletedAtToNodes < ActiveRecord::Migration
   def self.up
     add_column :nodes, :deleted_at, :datetime
-    
-    add_index :nodes, :deleted_at
+    add_index  :nodes, :deleted_at
+
+    Node.reset_column_information
+
+    require File.join(DevCMS.core_root, 'lib', 'update_all_and_delete_scope_fix.rb')
   end
 
   def self.down
