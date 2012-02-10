7b643b23332a2e5081c345bf416750a6d0a2e0d2
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-02-10 11:06:02 +0100

    Improve the date for the combined calendars controller.

--- app/controllers/combined_calendars_controller.rb
+++ app/controllers/combined_calendars_controller.rb
@@ -1,17 +1,19 @@
 # This +RESTful+ controller is used to orchestrate and control the flow of 
 # the application relating to +CombinedCalendar+ objects.
 class CombinedCalendarsController < ApplicationController
-  
+
   # The +show+ action needs a +CombinedCalendar+ object to work with.  
-  before_filter :find_combined_calendar, :only => [:show, :tomorrow]
-   
-  # * GET /combined_calendars/1
-  # * GET /combined_calendars/1.atom
-  # * GET /combined_calendars/1.xml
+  before_filter :find_combined_calendar, :only => [ :show, :tomorrow ]
+
+  # * GET /combined_calendars/:id
+  # * GET /combined_calendars/:id.atom
+  # * GET /combined_calendars/:id.xml
   def show
     respond_to do |format|
       format.html do
         @date = Date.parse(params[:date]) rescue Date.today
+        @date = Date.today if !@date.valid_gregorian_date?
+
         @calendar_items = @calendar.calendar_items.find_all_for_month_of(@date).group_by {|ci| ci.start_time.mday }
         render :template => 'calendars/show'
       end
@@ -22,22 +24,21 @@ class CombinedCalendarsController < ApplicationController
       format.xml { render :xml => @calendar }
     end
   end
-  
-  
-  # * GET /combined_calendars/1/tomorrow.atom
+
+  # * GET /combined_calendars/:id/tomorrow.atom
   def tomorrow
     tomorrow = Date.tomorrow
     conditions = [ '(start_time BETWEEN :start_time AND :end_time) OR (end_time BETWEEN :start_time AND :end_time)', { :start_time => tomorrow.beginning_of_day, :end_time => tomorrow.end_of_day } ]
     @calendar_items = @calendar.calendar_items.accessible.all(:include => :node, :conditions => conditions, :order => 'events.start_time DESC')
     @feed_title = I18n.t('calendars.tomorrow')
+
     respond_to do |format|
       format.atom do
         render :template => 'calendars/index', :layout => false
       end
     end
   end
-  
- 
+
 protected
 
   # Finds the +CombinedCalendar+ object corresponding to the passed in +id+ parameter.
