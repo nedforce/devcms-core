efb03dce7940b842f7521ac42851d57b716ea87b
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-02-15 16:17:24 +0100

    Add .xlsx and .pptx to the known icons for attachments.

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -2,20 +2,20 @@ module AttachmentsHelper
   # Returns the proper attachment icon based on the given +attachment+ extension.
   def attachment_icon(attachment, options = {})
     case attachment.extension
-    when "jpg", "jpeg", "gif", "png"
-      file_name = "image.png"
-    when "doc", "docx"
-      file_name = "word.png"
-    when "xls", "csv"
-      file_name = "excel.png"
-    when "ppt"
-      file_name = "powerpoint.png"
-    when "tar", "zip", "rar", "gz"
-      file_name = "compressed.png"
-    when "pdf", "ps"
-      file_name = "pdf.png"
+    when 'jpg', 'jpeg', 'gif', 'png'
+      file_name = 'image.png'
+    when 'doc', 'docx'
+      file_name = 'word.png'
+    when 'xls', 'xlsx', 'csv'
+      file_name = 'excel.png'
+    when 'ppt', 'pptx'
+      file_name = 'powerpoint.png'
+    when 'tar', 'zip', 'rar', 'gz'
+      file_name = 'compressed.png'
+    when 'pdf', 'ps'
+      file_name = 'pdf.png'
     else
-      file_name = "default.png"     
+      file_name = 'default.png'
     end
     image_tag("icons/mime_types/#{file_name}", { :alt => attachment.content_type }.merge(options))
   end
