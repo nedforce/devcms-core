b118a04f4b0a6a07243c7b623cac2d1a941c5481
Cas Ebbers <cjaebbers@gmail.com>
2012-04-26 12:19:27 +0200

    One more populate fix

--- defaults/db/seeds.rb
+++ defaults/db/seeds.rb
@@ -11,6 +11,6 @@ root_section = Site.create!(:title => 'Website', :description => 'Your website')
 root_section.node.update_attributes!(:layout => "default", :layout_variant => 'default', :layout_configuration => {'template_color'=>'default'})
 
 # Create an admin
-u = User.create!(:login => 'webmaster', :email_address => "webmaster@example.com", :password => 'admin', :password_confirmation => 'admin')
+u = PrivilegedUser.create!(:login => 'webmaster', :email_address => "webmaster@example.com", :password => 'admin', :password_confirmation => 'admin')
 u.update_attribute(:verified, true)
 u.give_role_on('admin', Node.root)
