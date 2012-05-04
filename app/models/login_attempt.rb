43acb3163d2c5abbd43911ba9dc5a6d34dd71c8a
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-05-04 10:42:08 +0200

    Fix compatability with Ruby 1.8.6.

--- app/models/login_attempt.rb
+++ app/models/login_attempt.rb
@@ -1,20 +1,22 @@
 class LoginAttempt < ActiveRecord::Base
-  
+
   validates_presence_of :ip
-  
+
   named_scope :failed, :conditions => { :success => false }
-  
+
   # Checks if the given +ip+ is blocked. If so, returns
   # the date of unblock. If not, returns +nil+.
   def self.is_ip_blocked?(ip)
     attempts = LoginAttempt.failed.all(:conditions => { :ip => ip, :created_at => Time.now.yesterday..Time.now })
-    return attempts.first.created_at.tomorrow if attempts.count >= 10
-    nil
+    if attempts.size >= 10
+      return attempts.first.created_at.tomorrow
+    else
+      nil
+    end
   end
-  
+
   def self.last_attempt_was_not_ten_seconds_ago(ip)
-    last_attempt = LoginAttempt.last(:conditions => {:ip => ip})
+    last_attempt = LoginAttempt.last(:conditions => { :ip => ip })
     (last_attempt.created_at > (Time.now - 10.seconds)) if last_attempt
   end
-  
 end
