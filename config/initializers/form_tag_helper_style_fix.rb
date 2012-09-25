1553bdc263167f52778b8c8181288ff129615097
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-09-25 15:54:12 +0200

    Webrichtlijnen fix for form tags styling.

---
+++ config/initializers/form_tag_helper_style_fix.rb
@@ -0,0 +1,25 @@
+require 'action_view/helpers/form_tag_helper'
+
+module ActionView::Helpers::FormTagHelper
+  private
+
+  def extra_tags_for_form(html_options)
+    authenticity_token = html_options.delete("authenticity_token")
+    method = html_options.delete("method").to_s
+
+    method_tag = case method
+      when /^get$/i # must be case-insensitive, but can't use downcase as might be nil
+        html_options["method"] = "get"
+        ''
+      when /^post$/i, "", nil
+        html_options["method"] = "post"
+        token_tag(authenticity_token)
+      else
+        html_options["method"] = "post"
+        method_tag(method) + token_tag(authenticity_token)
+    end
+
+    tags = utf8_enforcer_tag << method_tag
+    content_tag(:div, tags, :style => 'authenticity_token')
+  end
+end
