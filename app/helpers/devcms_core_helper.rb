6a96de070cb1d80d267d7dd397c7e2c17079fd49
Cas Ebbers <cjaebbers@gmail.com>
2012-10-15 16:25:43 +0200

    Use [alt] in header slideshow partial

--- app/helpers/devcms_core_helper.rb
+++ app/helpers/devcms_core_helper.rb
@@ -264,7 +264,7 @@ module DevcmsCoreHelper
             {
               :url   => big_header ? big_header_image_path(header_image, :format => :jpg) : header_image_path(header_image, :format => :jpg),
               :id    => "ss-image-#{header_image.id}",
-              :alt   => header_image.alt.to_s,
+              :alt   => header_image.alt,
               :title => header_image.title
             }
           end
