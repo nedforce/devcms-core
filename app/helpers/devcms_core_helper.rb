f5879a269e8b49ee0c1cb2b0b9b0b441358c24c6
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-10-23 10:27:06 +0200

    Use .to_s so we don't get nil alt's.

--- app/helpers/devcms_core_helper.rb
+++ app/helpers/devcms_core_helper.rb
@@ -257,14 +257,14 @@ module DevcmsCoreHelper
             {
               :url   => header_image,
               :id    => nil,
-              :alt   => nil,
+              :alt   => '',
               :title => nil
             }
           else
             {
               :url   => big_header ? big_header_image_path(header_image, :format => :jpg) : header_image_path(header_image, :format => :jpg),
               :id    => "ss-image-#{header_image.id}",
-              :alt   => header_image.alt,
+              :alt   => header_image.alt.to_s,
               :title => header_image.title
             }
           end
