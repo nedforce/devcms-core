e2f2a0bdd6a7367952760c28566d4b1ca3945156
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-11-08 15:17:41 +0100

    Make the opacity JavaScript way easier.

--- app/assets/javascripts/imports/opacity.js
+++ app/assets/javascripts/imports/opacity.js
@@ -1,13 +1,5 @@
-function setOpacity() {
-  $$('.carousel-wrapper .carousel-control').each(function (elem) {
-    $(elem).setStyle({
-      '-ms-filter': "progid:DXImageTransform.Microsoft.Alpha(Opacity=50)",
-      'filter': 'alpha(opacity=50)',
-      'opacity': '0.5'
-    });
-  });
-}
-
 document.observe('dom:loaded', function () {
-  setOpacity();
+  $$('.carousel-wrapper .carousel-control').each(function (elem) {
+    $(elem).setOpacity(0.5);
+  });
 });
