d1b8fe52819e8a09e91089123c41290bd2eca1fb
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-12-19 16:08:50 +0100

    Move readspeaker stuff to a separate helper + fix for #11419?

---
+++ app/helpers/readspeaker_helper.rb
@@ -0,0 +1,39 @@
+module ReadspeakerHelper
+
+  # Generates a button/link for readspeaker. A readspeaker id needs to be specified if multiple readspeak buttons are used on a page.
+  def readspeaker_button(options = {})
+    if Settler[:readspeaker_cid].present?
+      rid = options.delete(:rid)
+      link_class = [ options.delete(:class), 'readspeaker_link' ].compact.join(' ')
+      readspeaker_url = readspeaker_url_for(request.url, { :rid => rid }.merge(options))
+
+      content_tag(:div, :class => 'readspeaker_button') do
+        (image_tag('icons/sayit.png', :class => 'icon', :alt => '', :title => t('application.sayit_title')) +
+        (link_to(t('application.sayit'), readspeaker_url, { :title => t('application.sayit_title'), :class => link_class, :rel => 'nofollow' }.merge(options))) unless @node && (@node.hidden? || @node.private? || !@node.publishable?))
+      end
+    end
+  end
+
+  # Generates a block which will be read by readspeaker. For multiple blocks on a page specify a readspeaker_id.
+  def readspeaker(rid = nil, &block)
+    content = capture(rid, &block)
+
+    html = <<-html
+    <!-- RSPEAK_START -->
+    #{content}
+    <!-- RSPEAK_STOP -->
+    html
+
+    html = html.html_safe
+    (rid.blank? ? html : content_tag(:div, html, :id => "readspeaker_block_#{rid}"))
+  end
+
+  # Returns a url to readspeaker.
+  def readspeaker_url_for(target_url, options = {})
+    url  = "http://app.readspeaker.com/cgi-bin/rsent?customerid=#{Settler[:readspeaker_cid]}"
+    url << "&amp;readid=readspeaker_block_#{options[:rid]}" if options[:rid].present?
+    url << "&amp;lang=#{options[:lang] || 'nl'}"
+    url << "&amp;url=#{CGI.escape(target_url)}"
+    url.html_safe
+  end
+end
