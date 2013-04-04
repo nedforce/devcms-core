5468668914bed68b9fb8bf4dafd7c50979103bea
Reinier de Lange <r.j.delange@nedforce.nl>
2013-04-04 11:17:21 +0200

    change the scope of the nodes to be checked

--- lib/devcms_core/content_node_checker_runner.rb
+++ lib/devcms_core/content_node_checker_runner.rb
@@ -2,7 +2,7 @@ module DevcmsCore
   class ContentNodeCheckerRunner < DataChecker::Runner
     models      *DevcmsCore::Engine.registered_content_types.map(&:constantize)
     checker     LinkChecker  
-    scope       lambda{|model| model.joins(:node) }
+    scope       lambda{|model| model.joins(:node).where('nodes.last_checked_at is null or nodes.last_checked_at < ?', 1.week.ago) }
     after_check lambda{|subject| subject.node.update_column :last_checked_at, Time.now }  
   end
 end
