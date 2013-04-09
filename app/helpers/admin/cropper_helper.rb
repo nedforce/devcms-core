c3321f3bcb7d2d14b74e3e3ca65f843936225373
Roel van der Hoorn <vanderhoorn@gmail.com>
2013-04-09 14:56:33 +0200

    Use more readable (less evil) code.

--- app/helpers/admin/cropper_helper.rb
+++ app/helpers/admin/cropper_helper.rb
@@ -3,9 +3,10 @@ module Admin::CropperHelper
   def render_cropper(image, html_options = nil)
     html_options ||= {}
     html_options.merge!(:id => "image_cropper_#{image.id}")
-    
-    locals = { :image => image, :is_header => is_header = image.node.parent.content_type == 'NewsItem' }  
-      
+
+    is_header = image.node.parent.content_type == 'NewsItem'
+    locals = { :image => image, :is_header => is_header }  
+
     content_tag :div, html_options do
       if image.orientation == :vertical || is_header
         render :partial => 'cropper_vertical', :locals => locals
