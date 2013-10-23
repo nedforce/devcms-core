1090f5232cf85a8e7f5540c880944024c0e3da24
Peter Leppers <p.leppers@nedforce.nl>
2013-10-23 16:12:26 +0200

    Added icon helper form deventer

---
+++ app/helpers/icon_helper.rb
@@ -0,0 +1,20 @@
+module IconHelper
+  
+  def icon_tag name, options={}
+    options[:class] = ["deventer-icon-#{name}", options[:class]].compact
+    content_tag :span, '', options
+  end
+
+  def top_task_icon_tag name, options = {}
+    options[:data] = options[:data] ? options.data.merge!({icon: name}) : {icon: name}
+    content_tag :span, '', options
+  end
+
+  def icon_tag_link_to icon_name, url, options= {}
+    title = options.delete :title
+    icon_options = options.delete(:icon_options) || {}
+    link_to [icon_tag(icon_name, icon_options), title].compact.join(' ').html_safe, url, options
+
+  end
+
+end
