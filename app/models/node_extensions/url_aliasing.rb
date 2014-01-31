5656d996b2172432298b315b2633ad557e662b44
Arthur Holstvoogd <a.holstvoogd@nedforce.nl>
2014-01-31 15:14:11 +0100

    Touch node after url_alias update

--- app/models/node_extensions/url_aliasing.rb
+++ app/models/node_extensions/url_aliasing.rb
@@ -122,6 +122,7 @@ module NodeExtensions::UrlAliasing
     Node.sort_by_ancestry(self_and_descendants).each do |node|
       node.update_column(:url_alias, node.generate_unique_url_alias)
       node.update_column(:custom_url_alias, node.generate_unique_custom_url_alias) if node.custom_url_suffix.present?
+      node.touch
     end
   end
   
