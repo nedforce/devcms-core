ac2d459ec5ea39f09bde80eb3928d1dcb444d1d7
Roel van der Hoorn <vanderhoorn@gmail.com>
2014-10-09 10:41:29 +0200

    Make the version of libxml-ruby a little more liberal, so we can deploy to railscluster.

--- config/environment.rb
+++ config/environment.rb
@@ -12,20 +12,20 @@ else
   config.gem 'rmagick',             :version => '>=2.12.2', :lib => 'RMagick'
 end
 
-config.gem 'acts-as-taggable-on',   :version => '2.0.3',                              :source => "http://gemcutter.org"
-config.gem 'addressable',           :version => "~> 2.1",   :lib => 'addressable/uri'
+config.gem 'acts-as-taggable-on',   :version => '2.0.3',                              :source => 'http://gemcutter.org'
+config.gem 'addressable',           :version => '~> 2.1',   :lib => 'addressable/uri'
 config.gem 'ancestry',              :version => '~> 1.3.0'
 config.gem 'dsl_accessor',          :version => '0.3.3'
-config.gem 'dynamic_attributes',    :version => '~> 1.1.3'  
+config.gem 'dynamic_attributes',    :version => '~> 1.1.3'
 config.gem 'fastercsv'
 config.gem 'feed-normalizer',       :version => '~> 1.5.2'
 config.gem 'haml',                  :version => '~> 3.0'
-config.gem 'libxml-ruby',           :version => "~> 2.2.0", :lib => 'libxml'
+config.gem 'libxml-ruby',           :version => '~> 2.2',   :lib => 'libxml'
 config.gem 'pg',                    :version => '~> 0.11.0'
 config.gem 'settler',               :version => '~> 1.2.3'
-config.gem 'shuber-sortable',       :version => "~> 1.0.6", :lib => 'sortable',       :source => "http://gems.github.com"
+config.gem 'shuber-sortable',       :version => '~> 1.0.6', :lib => 'sortable',       :source => 'http://gems.github.com'
 # use the plugin instead, shuber isn't available as gem anymore
 config.gem 'tidy',                  :version => '1.1.2'
 config.gem 'whenever',              :version => '>= 0.4', :lib => false
 config.gem 'spreadsheet',           :version => '~> 0.6.5.2'
-config.gem "roo",                   :version => "~> 1.10.1"
+config.gem 'roo',                   :version => '~> 1.10.1'
