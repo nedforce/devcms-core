37281af41ec30143ebf237a4d43d31f244c8c010
Roel van der Hoorn <vanderhoorn@gmail.com>
2015-01-29 13:38:51 +0100

    Set the @host variable in the faq suggestions mailer, so it shows the host in the signature.

--- app/mailers/faq_suggestions_mailer.rb
+++ app/mailers/faq_suggestions_mailer.rb
@@ -1,13 +1,14 @@
 class FaqSuggestionsMailer < ActionMailer::Base
-  def faq_suggestion(faq_suggestion)
+  def faq_suggestion(faq_suggestion, options = {})
     @recipients            = Settler[:faq_mail_recipient]
     @from                  = faq_suggestion.email || Settler[:mail_from_address]
-    @sent_on               = Time.now
+    @sent_on               = Time.zone.now
     @subject               = t('faqs.suggestion_for', theme: faq_suggestion.theme.title)
+    @host                  = options[:host] || Settler[:host]
     @question              = faq_suggestion.question
     @explanation           = faq_suggestion.explanation
-    @url                   = admin_nodes_path(:active_node_id => faq_suggestion.theme.node.id)
+    @url                   = admin_nodes_path(active_node_id: faq_suggestion.theme.node.id)
 
-    mail(:from => @from, :to => @recipients, :subject => @subject)
+    mail(from: @from, to: @recipients, subject: @subject)
   end
 end
