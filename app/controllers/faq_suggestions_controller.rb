5a029415822c7f8dc583f240f990ffcb723d2fe5
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2015-05-21 09:12:59 +0200

    Ticket #21821
    Validate attributes in a faq suggestions.

--- app/controllers/faq_suggestions_controller.rb
+++ app/controllers/faq_suggestions_controller.rb
@@ -5,22 +5,49 @@ class FaqSuggestionsController < ApplicationController
   end
 
   def create
-    if FaqSuggestionsMailer.faq_suggestion(OpenStruct.new(
+    if suggestion_valid? &&
+      FaqSuggestionsMailer.faq_suggestion(OpenStruct.new(
           theme:        @theme,
           question:     params[:question],
           explanation:  params[:explanation],
           email:  params[:email]
-        )).deliver
+      )).deliver
+
       flash[:notice] = t('faq_suggestions.delivery_succeeded')
       redirect_to content_node_url(@theme)
     else
-      flash[:error] = t('faq_suggestions.delivery_failed')
-      redirect_to :back
+      flash[:error] = (t('faq_suggestions.delivery_failed') + render_error_list).html_safe
+      render :new
     end
   end
 
   private
   def find_theme
     @theme = FaqTheme.find(params[:theme_id])
+  end
+
+  def suggestion_valid?
+    @errors = []
+    validate_presence; validate_email
+    @errors.none?
+  end
+
+  def render_error_list
+    error_list = @errors.map { |error| "<li> #{error} </li>" }.join.html_safe
+    "<ul> #{error_list} </ul>".html_safe
+  end
+
+  def validate_presence
+    [:question, :explanation, :email].each do |attr|
+      if params[attr].blank?
+        @errors << t('faq_suggestions.errors.presence', attribute: t(attr, scope: 'faq_suggestions'))
+      end
+    end
+  end
+
+  def validate_email
+    unless params[:email] =~ EmailValidator::REGEX
+      @errors << t('faq_suggestions.errors.email_invalid')
+    end
   end
 end
