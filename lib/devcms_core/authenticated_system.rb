9ebf5e5d5f81f9e119606806ec4ecfe8993eda30
Roel van der Hoorn <vanderhoorn@gmail.com>
2015-06-04 15:42:27 +0200

    Fix the authentication IP mismatch logging (resolves #22082).

--- lib/devcms_core/authenticated_system.rb
+++ lib/devcms_core/authenticated_system.rb
@@ -151,7 +151,9 @@ module DevcmsCore
 
       def ip_violation(ip)
         if ip != request.remote_ip
-          Rails.logger.info "#{Time.zone.now} AUTHENTICATION IP MISMATCH: For user (#{User.find_by_id(session[:user_id]).try(:login)}). " +
+          mismatched_user = session[:user_id] ? User.find_by_id(session[:user_id]) : User.find_by_remember_token(cookies[:auth_token])
+
+          Rails.logger.info "#{Time.zone.now} AUTHENTICATION IP MISMATCH: For user (#{mismatched_user.try(:login)}). " +
             "IP should be (#{ip}), IP was (#{request.remote_ip})"
           true
         else
