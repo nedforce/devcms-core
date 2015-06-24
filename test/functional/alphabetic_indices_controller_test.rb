3596e6cc71332b8c42a0cf717c4ba1edb11115bb
Roel van der Hoorn <vanderhoorn@gmail.com>
2015-06-24 21:03:36 +0200

    Cleanup the alphabetic indices controller test.

--- test/functional/alphabetic_indices_controller_test.rb
+++ test/functional/alphabetic_indices_controller_test.rb
@@ -1,18 +1,21 @@
 require File.expand_path('../../test_helper.rb', __FILE__)
 
+# Functional tests for the +AlphabeticIndicesController+.
 class AlphabeticIndicesControllerTest < ActionController::TestCase
   self.use_transactional_fixtures = true
 
-  def test_should_show_alphabetic_index
-    get :show, :id => alphabetic_indices(:root_alphabetic_index)
+  test 'should show alphabetic index' do
+    get :show, id: alphabetic_indices(:root_alphabetic_index)
+
     assert_response :success
     assert assigns(:alphabetic_index)
     assert assigns(:items)
     assert_equal nodes(:root_alphabetic_index_node), assigns(:node)
   end
 
-  def test_should_show_alphabetic_index_by_letter
-    get :letter, :id => alphabetic_indices(:root_alphabetic_index), :letter => 'A'
+  test 'should show alphabetic index by letter' do
+    get :letter, id: alphabetic_indices(:root_alphabetic_index), letter: 'A'
+
     assert_response :success
     assert assigns(:alphabetic_index)
     assert assigns(:items)
