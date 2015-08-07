ca637b8d786f78517d16c9f871412021b9891166
Roel van der Hoorn <vanderhoorn@gmail.com>
2015-08-07 21:24:02 +0200

    Refactor the admin cropper helper.

--- app/helpers/admin/cropper_helper.rb
+++ app/helpers/admin/cropper_helper.rb
@@ -1,18 +1,14 @@
 module Admin::CropperHelper
-
   def render_cropper(image, html_options = nil)
     html_options ||= {}
-    html_options.merge!(:id => "image_cropper_#{image.id}")
-    
-    locals = { :image => image }  
-      
+    html_options[:id] = "image_cropper_#{image.id}"
+
     content_tag :div, html_options do
-      if image.orientation == :vertical
-        render :partial => 'cropper_vertical', :locals => locals
-      else
-        render :partial => 'cropper_horizontal', :locals => locals
-      end
+      render partial: cropper_partial(image.orientation), locals: { image: image }
     end
   end
 
+  def cropper_partial(orientation)
+    orientation == :vertical ? 'cropper_vertical' : 'cropper_horizontal'
+  end
 end
