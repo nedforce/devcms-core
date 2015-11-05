cfe9127aed3f8fd16fc705c50bce4378c04d474a
Roel van der Hoorn <vanderhoorn@gmail.com>
2015-11-05 14:47:37 +0100

    Give the honeypot text fields a title for the Webrichtlijnen (#23340, #23473).

--- app/helpers/honeypot_helper.rb
+++ app/helpers/honeypot_helper.rb
@@ -1,5 +1,5 @@
 module HoneypotHelper
   def render_honeypot
-    (text_field_tag(Rails.application.config.honeypot_empty_name, '', :class => Rails.application.config.honeypot_class) + text_field_tag(Rails.application.config.honeypot_name, Rails.application.config.honeypot_value, :class => Rails.application.config.honeypot_class)).html_safe
+    (text_field_tag(Rails.application.config.honeypot_empty_name, '', title: 'Leeg laten', class: Rails.application.config.honeypot_class) + text_field_tag(Rails.application.config.honeypot_name, Rails.application.config.honeypot_value, title: 'Niet wijzigen', class: Rails.application.config.honeypot_class)).html_safe
   end
 end
