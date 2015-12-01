f3109ab05369273a3fc1b95d9b03941c04923f65
Eelco Osnabrugge <e.osnabrugge@nedforce.nl>
2015-12-01 11:20:54 +0100

    increase max size of uploaded image to 2048x2048

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,7 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded.
   #     process :scale => [200, 300]
 
-  process resize_to_limit: [1024, 1024]
+  process resize_to_limit: [2048, 2048]
 
   #
   #     def scale(width, height)
