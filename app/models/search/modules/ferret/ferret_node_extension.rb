42a835a89a9f360f4686f9f38f237fd8fcbc8b09
Roel van der Hoorn <vanderhoorn@gmail.com>
2016-02-09 11:48:20 +0100

    Ensure ancestry_to_index does not raise an error in the ferret logs.

--- app/models/search/modules/ferret/ferret_node_extension.rb
+++ app/models/search/modules/ferret/ferret_node_extension.rb
@@ -38,7 +38,7 @@ module Search::Modules::Ferret::FerretNodeExtension
     end
 
     def ancestry_to_index
-      "XX#{ancestry.gsub(/\//, 'X')}X"
+      "XX#{ancestry.to_s.gsub(/\//, 'X')}X"
     end
 
     def is_hidden_to_index
