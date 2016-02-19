b4dd9746621d7ff740ee9e5f9aa90fe3fbef85d1
Eelco Osnabrugge <e.osnabrugge@nedforce.nl>
2016-02-19 14:49:14 +0100

    Fix Faq-test (use title instead of question)

--- test/unit/faq_test.rb
+++ test/unit/faq_test.rb
@@ -5,14 +5,14 @@ class FaqTest < ActiveSupport::TestCase
     assert_difference "FaqArchive.count" do
       FaqArchive.create(:parent => Node.root, :title => 'FAQ', :description => 'Vragen en antwoorden').tap do |fa|
         assert !fa.new_record?, fa.errors.full_messages.join
-        Faq.create(:parent => fa.node, :question => 'Vraag', :answer => 'Antwoord').tap do |faq|
+        Faq.create(:parent => fa.node, :title => 'Vraag', :answer => 'Antwoord').tap do |faq|
           assert !faq.valid?, faq.errors.full_messages.join
           assert faq.new_record?, faq.errors.full_messages.join
         end
         assert_difference "FaqTheme.count" do
           FaqTheme.create(:parent => fa.node, :title => "Faq Thema").tap do |ft|
             assert !ft.new_record?, ft.errors.full_messages.join
-            Faq.create(:parent => ft.node, :question => 'Vraag', :answer => 'Antwoord').tap do |faq|
+            Faq.create(:parent => ft.node, :title => 'Vraag', :answer => 'Antwoord').tap do |faq|
               assert !faq.valid?, faq.errors.full_messages.join
               assert faq.new_record?, faq.errors.full_messages.join
             end
@@ -20,7 +20,7 @@ class FaqTest < ActiveSupport::TestCase
               FaqCategory.create(:parent => ft.node, :title => "Faq Categorie").tap do |fc|
                 assert !fc.new_record?, fc.errors.full_messages.join
                 assert_difference "Faq.count" do
-                  Faq.create(:parent => fc.node, :question => 'Vraag', :answer => 'Antwoord').tap do |faq|
+                  Faq.create(:parent => fc.node, :title => 'Vraag', :answer => 'Antwoord').tap do |faq|
                     assert faq.valid?, faq.errors.full_messages.join
                     assert !faq.new_record?, faq.errors.full_messages.join
                   end
