7d423fe858cb67d8f52b93e5193b822e658cc4ea
Roel van der Hoorn <vanderhoorn@gmail.com>
2016-03-10 18:19:27 +0100

    Cleanup the admin layouts controller.

--- app/controllers/admin/layouts_controller.rb
+++ app/controllers/admin/layouts_controller.rb
@@ -1,7 +1,7 @@
 class Admin::LayoutsController < Admin::AdminController
-  before_filter :find_node, :only => [:edit, :variants_settings_and_targets, :targets, :update]
+  before_filter :find_node, only: [:edit, :variants_settings_and_targets, :targets, :update]
   before_filter :find_layout
-  before_filter :find_current_sortlets, :only => [:edit, :targets, :variants_settings_and_targets]
+  before_filter :find_current_sortlets, only: [:edit, :targets, :variants_settings_and_targets]
 
   layout false
 
@@ -13,27 +13,28 @@ class Admin::LayoutsController < Admin::AdminController
     #               "targets" => { "primary_column" => ["1"], "main_content_column" => ["4","5"], "secondary_column" => [nil] } }
     if params['node'].present?
       set_footer_links
-      success = @node.update_layout(:node => params['node'], :targets => params['targets']) rescue false
+      success = @node.update_layout(node: params['node'], targets: params['targets']) rescue false
+
       if success
-        render :text => 'ok', :status => :ok
+        render text: 'ok', status: :ok
       else
-        render :text => 'not ok', :status => :precondition_failed
+        render text: 'not ok', status: :precondition_failed
       end
-    else
-      render :text => 'ok', :status => :ok if @node.reset_layout
+    elsif @node.reset_layout
+      render text: 'ok', status: :ok
     end
   end
 
   def variants_settings_and_targets
     if @layout.present?
-      render :partial => 'variants_settings_and_targets'
+      render partial: 'variants_settings_and_targets'
     else
-      render :nothing => true
+      render nothing: true
     end
   end
 
   def targets
-    render :partial => 'targets'
+    render partial: 'targets'
   end
 
   protected
@@ -44,6 +45,7 @@ class Admin::LayoutsController < Admin::AdminController
 
   def find_layout
     @layout = Layout.find(params[:id]) || @node.own_or_inherited_layout
+
     if @layout.present?
       @variant = @layout.find_variant(params[:variant_id]) || @node.own_or_inherited_layout_variant
     end
@@ -56,12 +58,12 @@ class Admin::LayoutsController < Admin::AdminController
     # Setup the custom representations
     @current_sortlets[:custom_representations] = @node.own_or_inherited_layout.custom_representations.map do |type, config|
       {
-        :title                 => I18n.t("#{@node.content_type.tableize}.#{type}", :default => config['name']),
-        :representation        => config['representation'],
-        :custom_representation => true,
-        :nodeId                => type,
-        :hideClose             => true,
-        :xtype                 => 'sortlet'
+        title:                 I18n.t("#{@node.content_type.tableize}.#{type}", default: config['name']),
+        representation:        config['representation'],
+        custom_representation: true,
+        nodeId:                type,
+        hideClose:             true,
+        xtype:                 'sortlet'
       }
     end
 
@@ -70,21 +72,21 @@ class Admin::LayoutsController < Admin::AdminController
         if el.custom_type.present?
           ct = @current_sortlets[:custom_representations].select { |ct| ct[:nodeId] == el.custom_type }.first
           @current_sortlets[:custom_representations].delete(ct)
-          { :id => el.id }.merge(ct)
+          { id: el.id }.merge(ct)
         else
           {
-            :title  => el.content.content.content_title,
-            :id     => el.id,
-            :node   => el.content.to_tree_node_for(current_user),
-            :nodeId => el.content.id,
-            :xtype  => 'sortlet'
+            title:  el.content.content.content_title,
+            id:     el.id,
+            node:   el.content.to_tree_node_for(current_user),
+            nodeId: el.content.id,
+            xtype:  'sortlet'
           }
         end
       end
 
       if config['main_content'] == true && @node.content_type == 'Section' && @node.content.frontpage_node.present?
         front_page_node = @node.content.frontpage_node
-        @current_sortlets[target] = [{ :title => front_page_node.content.content_title, :id => front_page_node.id, :nodeId => front_page_node.id, :data => { :node => front_page_node.to_tree_node_for(current_user) }, :xtype => 'sortlet' }]
+        @current_sortlets[target] = [{ title: front_page_node.content.content_title, id: front_page_node.id, nodeId: front_page_node.id, data: { node: front_page_node.to_tree_node_for(current_user) }, xtype: 'sortlet' }]
       end
     end
   end
@@ -93,9 +95,8 @@ class Admin::LayoutsController < Admin::AdminController
     if params[:node]
       footer_links = params[:node][:layout_configuration][:footer_links]
       if footer_links
-        params[:node][:layout_configuration][:footer_links] = footer_links.select { |index, link| link[:text].present? && link[:url].present? }
+        params[:node][:layout_configuration][:footer_links] = footer_links.select { |_index, link| link[:text].present? && link[:url].present? }
       end
     end
   end
-
 end
