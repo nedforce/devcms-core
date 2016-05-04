6c8ec20fda0b1c3f9f02d2c927a662ff4ed20a05
Roel van der Hoorn <vanderhoorn@gmail.com>
2016-05-04 17:45:22 +0200

    Fix the +send_registration_notifications+ method on events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -61,7 +61,7 @@ class Event < ActiveRecord::Base
   end
 
   def self.send_registration_notifications
-    all(conditions: ['start_time <= ? AND subscription_enabled = ?', Time.now + 1.day, true]).each do |event|
+    where('start_time <= ? AND subscription_enabled = ?', Time.now + 1.day, true).each do |event|
       event.update_attribute :subscription_enabled, false
       EventMailer.event_registrations(event).deliver_now if event.event_registrations.any?
     end
